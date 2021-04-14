ca9cc08a202e06f8e5cd59384b2dc2a8efd3c8c1
Amber Hsiao <cs8430s1001215@gmail.com>
2021-04-14 16:29:13 +0800

    new version

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=reply))
+        TextSendMessage(text='hihihihi'))
 
 
 if __name__ == "__main__":
